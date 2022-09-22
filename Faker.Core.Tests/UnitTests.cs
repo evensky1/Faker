8337ccb51f5002ecea37da93f9b54f8163341a85
evensky1 <fromthedeepsize@gmail.com>
2022-09-22 20:48:30 +0300

    New unit tests was added

--- Faker.Core.Tests/UnitTests.cs
+++ Faker.Core.Tests/UnitTests.cs
@@ -30,6 +30,7 @@ public class Tests
             Assert.That(typeof(double), Is.EqualTo(_faker.Create<double>().GetType()));
             Assert.That(typeof(string), Is.EqualTo(_faker.Create<string>().GetType()));
             Assert.That(typeof(char), Is.EqualTo(_faker.Create<char>().GetType()));
+            Assert.That(typeof(DateTime), Is.EqualTo(_faker.Create<DateTime>().GetType()));
         });
     }
 
@@ -37,5 +38,63 @@ public class Tests
     public void Not_Supported_Type()
     {
         Assert.Throws<NotImplementedException>(() => _faker.Create<Tests>());
+    }
+
+    [Test]
+    public void Arithmetic_Operations_With_Integers()
+    {
+        long a = _faker.Create<long>();
+        int b = _faker.Create<int>();
+        short c = _faker.Create<short>();
+        byte d = _faker.Create<byte>();
+        Assert.DoesNotThrow(() =>
+        {
+            var l = a + b + c + d;
+        });
+    }
+    
+    [Test]
+    public void Arithmetic_Operations_With_Unsigned_Integers()
+    {
+        ulong a = _faker.Create<ulong>();
+        uint b = _faker.Create<uint>();
+        ushort c = _faker.Create<ushort>();
+        Assert.DoesNotThrow(() =>
+        {
+            var l = a + b + c;
+        });
+    }
+
+    [Test]
+    public void Arithmetic_Operations_With_Reals()
+    {
+        double a = _faker.Create<double>();
+        float b = _faker.Create<float>();
+        
+        Assert.DoesNotThrow(() =>
+        {
+            var d = a + b;
+        });
+    }
+    
+    [Test]
+    public void String_Generation()
+    {
+        Assert.DoesNotThrow(() =>
+        {
+            string str = _faker.Create<string>();
+            str = str.Trim();
+            Console.WriteLine(str);
+        });
+    }
+
+    [Test]
+    public void DateTime_Generation_By_Format()
+    {
+        Assert.DoesNotThrow(() =>
+        {
+            DateTime dateTime = _faker.Create<DateTime>();
+            Console.WriteLine(dateTime.ToString("dddd, dd MMMM yyyy HH:mm:ss"));
+        });
     }
 }
